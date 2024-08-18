bb9f28824a87b7e2417323268d66052730ad7788
TranTrungTinLV <trungtintin1989@gmail.com>
2024-08-19 02:59:10 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/cookingandrecipe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cookingandrecipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cooking And Recipe'),
+        ),
+        body: Text('Cooking'),
+      ),
+    );
+  }
+}
